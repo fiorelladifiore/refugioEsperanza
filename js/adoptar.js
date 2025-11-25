48f22968fd898adb0447c0c2924ecff08f851abf
DAleMF05 <ale_machado0511@hotmail.com>
2025-11-25 13:14:18 -0300

    Agregado de paginacion, correciones varias

--- js/adoptar.js
+++ js/adoptar.js
@@ -1,5 +1,6 @@
 "use strict"
 
+//Manejo de filtros
 const btnFiltros = document.getElementById("btnFiltros");
 const menuFiltros = document.getElementById("menuFiltros");
 const icono = document.querySelector(".icono-filtros");
@@ -7,9 +8,6 @@ const icono = document.querySelector(".icono-filtros");
 btnFiltros.addEventListener("click", () => {
     menuFiltros.classList.toggle("activo");
 });
-
-
-
 
 
 const checkboxes = document.querySelectorAll('.menu-filtros input[type="checkbox"]');
@@ -22,3 +20,37 @@ function actualizarContador() {
 
 // Cada vez que se selecciona o des-selecciona un filtro:
 checkboxes.forEach(cb => cb.addEventListener("change", actualizarContador));
+
+
+
+
+//Manejo de paginacion
+const numBtns = document.querySelectorAll(".pag-btn.num");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+function setActivePage(index) {
+    numBtns.forEach(b => b.classList.remove("active"));
+    numBtns[index].classList.add("active");
+}
+
+numBtns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        setActivePage(i);
+    });
+});
+
+prevBtn.addEventListener("click", () => {
+    let activeIndex = [...numBtns].findIndex(b => b.classList.contains("active"));
+    if (activeIndex > 0) {
+        setActivePage(activeIndex - 1);
+    }
+});
+
+nextBtn.addEventListener("click", () => {
+    let activeIndex = [...numBtns].findIndex(b => b.classList.contains("active"));
+    if (activeIndex < numBtns.length - 1) {
+        setActivePage(activeIndex + 1);
+    }
+});
+
